474a3cccb475e39c3056f47707162c3abd942eed
Manuele523 <exacon2019@gmail.com>
2019-10-22 21:52:51 +0200

    tuesday22October

--- src/main/java/it/library/controller/AppController.java
+++ src/main/java/it/library/controller/AppController.java
@@ -4,10 +4,8 @@ import it.library.dto.Author;
 import it.library.dto.Book;
 import it.library.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
