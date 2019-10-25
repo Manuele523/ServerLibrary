5f77a73815272a883707578a2c3dff5d87171dfe
Manuele523 <exacon2019@gmail.com>
2019-10-25 17:29:32 +0200

    25October

--- src/main/java/it/library/controller/AppController.java
+++ src/main/java/it/library/controller/AppController.java
@@ -6,7 +6,6 @@ import it.library.dto.Type;
 import it.library.service.BookService;
 import it.library.service.TypeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
